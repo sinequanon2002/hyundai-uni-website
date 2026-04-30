ac42f770846d0392bb44caf885f66ed2864785e7
sinequanon2002 <zeal4one@gmail.com>
2026-04-30 22:48:16 +0900

    chore: update company information in constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,12 @@
 export const COMPANY = {
-  name: "[회사명]환경 주식회사", // TODO: 실제 회사명
-  shortName: "[회사명]환경",
-  ceo: "○○○",
-  established: "2015-XX-XX",
-  businessNumber: "000-00-00000",
-  licenseNumber: "○○도 제 0000-0호",
-  address: "경상남도 ...",
-  tel: "055-000-0000",
-  fax: "055-000-0000",
-  email: "info@company.co.kr",
-  businessHours: "평일 09:00 - 18:00"
+  name: "주식회사 현대유앤아이", // TODO: 실제 회사명
+  shortName: "주식회사 현대유앤아이",
+  ceo: "양솔빈",
+  established: "2024-02-14",
+  businessNumber: "857-87-03200",
+  licenseNumber: "허가번호 제 대구수집 130호",
+  address: "경상북도 경산시 하양읍 하양로 34, 2층 비-04호",
+  tel: "010-9084-9480",
+  email: "hduni3973@naver.com",
+  businessHours: "평일 08:00 - 18:00"
 };
